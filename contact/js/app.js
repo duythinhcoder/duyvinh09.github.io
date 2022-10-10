961b5679cd4593a4ff1c598109ba6bdd3eeb4000
Đinh Duy Vinh <85325084+dinhduyvinh@users.noreply.github.com>
2022-10-11 05:04:04 +0700

    Add files via upload

---
+++ contact/js/app.js
@@ -0,0 +1,24 @@
+﻿const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+
+console.log(
+  "%c Coder : Nguy?n Quang Hung %c",
+  'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
+  "font-size:12px;color:#999999;"
+);
