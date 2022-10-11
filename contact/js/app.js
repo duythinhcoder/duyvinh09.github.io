f61a87645ac8f931fe235a2096b9c99cf6e72e05
Đinh Duy Vinh <85325084+dinhduyvinh@users.noreply.github.com>
2022-10-11 13:42:27 +0700

    Update app.js

--- contact/js/app.js
+++ contact/js/app.js
@@ -18,7 +18,7 @@ inputs.forEach((input) => {
 });
 
 console.log(
-  "%c Coder : Nguy?n Quang Hung %c",
+  "%c Coder : Đinh Duy Vinh %c",
   'font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;font-size:24px;color:#00bbee;-webkit-text-fill-color:#00bbee;-webkit-text-stroke: 1px #00bbee;',
   "font-size:12px;color:#999999;"
 );
